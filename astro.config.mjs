e3eeb59bfbcbc0374bcf19743fae449baca158f9
PenguinCodes <34519990+PenguinCodes@users.noreply.github.com>
2025-06-30 00:41:56 +0530

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio-2025/', 
+  base: '/portfolio-2025/',
+  build: {
+    hooks: {
+      done() {
+        fs.writeFileSync(new URL('./dist/.nojekyll', import.meta.url), '');
+      }
+    }
+  },
   integrations: [tailwind(), react()],
 });
