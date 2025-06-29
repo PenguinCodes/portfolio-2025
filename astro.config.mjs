5cafc5cb23ed77dcfa50d7d92c99f18c74701595
PenguinCodes <34519990+PenguinCodes@users.noreply.github.com>
2025-06-30 01:02:05 +0530

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import fs from "fs";
 export default defineConfig({
-  base: "/portfolio-2025/",
-  build: {
-    hooks: {
-      done() {
-        fs.writeFileSync(new URL("./dist/.nojekyll", import.meta.url), "");
-      },
-    },
-  },
-  integrations: [tailwind(), react()],
+  site: 'https://penguincodes.github.io',
+  base: 'portfolio-2025',
+  integrations: [tailwind(), react()]
 });
